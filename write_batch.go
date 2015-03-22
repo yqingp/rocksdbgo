92fececaab3aee755c70b761ce11a480c458e9a5
yqingp <yqingp@gmail.com>
2015-03-22 17:24:17 +0800

    add write_batch support

--- write_batch.go
+++ write_batch.go
@@ -7,6 +7,36 @@ package rocksdbgo
 */
 import "C"
 
+import (
+	"errors"
+)
+
 type WriteBatch struct {
-	WriteBatch *C.rocksdb_writebatch_t
+	writeBatch *C.rocksdb_writebatch_t
+}
+
+/*
+extern rocksdb_writebatch_t* rocksdb_writebatch_create();
+*/
+func NewWriteBatch() {
+	return &WriteBatch{
+		writeBatch: C.rocksdb_writebatch_create(),
+	}
+}
+
+/*
+extern void rocksdb_writebatch_put(rocksdb_writebatch_t*,const char* key, size_t klen,const char* val, size_t vlen);
+*/
+func (w *WriteBatch) Put(key []byte, value []byte) {
+	var errInfo *C.char
+
+	k, v := C.CString(string(key)), C.CString(string(value))
+	defer func() {
+		C.free(unsafe.Pointer(k))
+		C.free(unsafe.Pointer(v))
+	}()
+
+	C.rocksdb_writebatch_put(w.writeBatch, k, C.size_t(len(key)), v, C.size_t(len(value)))
+
+	return nil
 }
